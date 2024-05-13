832f1e667e09ca4ec297a7f25881b5c5646b8aa2
Yeojin Song <41974994+Yeojinsong@users.noreply.github.com>
2024-05-13 15:21:31 +1000

    line charts

--- js/charts.js
+++ js/charts.js
@@ -1,54 +1,75 @@
 function init() {
-    var w = 700;
-    var h = 500;
-    var padding = 30; // Added padding for axes
+    const margin = { top: 30, right: 30, bottom: 70, left: 60 },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
-    var svg = d3.select("#chart1")
+    const svg = d3.select("#chart1")
         .append("svg")
-        .attr("width", w)
-        .attr("height", h);
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // Parse the CSV data
     d3.csv("../health.csv").then(function (data) {
-        const australiaData = data.filter(d => d.Country === "Australia" && d.VAR === "BODYVBMS");
-        // Prepare the data for the line chart
-        const parsedData = australiaData.map(d => ({
-            year: +d.Year,
-            measure: d.Measure,
-            value: +d.Value
-        }));
+        const selectedCountries = ['Australia', 'Canada', 'Korea', 'Brazil', 'Russia'];
+        const variable = 'FOODTFAT'; // Variable for the line chart
+
+        // Filter data for selected countries and variable
+        const filteredData = data.filter(d => selectedCountries.includes(d.Country) && d.VAR === variable);
+
+        // Parse year as date
+        const parseTime = d3.timeParse("%Y");
+        filteredData.forEach(function (d) {
+            d.Year = parseTime(d.Year);
+            d.Value = +d.Value;
+        });
+
+        // Define color scale
+        const colorScale = d3.scaleOrdinal()
+            .domain(selectedCountries)
+            .range(d3.schemeCategory10); // Or any other color scheme you prefer
 
         // Create scales
-        const xScale = d3.scaleBand()
-            .domain(parsedData.map(d => d.year))
-            .range([padding, w - padding])
-            .padding(0.2);
+        const xScale = d3.scaleTime()
+            .domain(d3.extent(filteredData, d => d.Year))
+            .range([0, width]);
 
         const yScale = d3.scaleLinear()
-            .domain([0, d3.max(parsedData, d => d.value)])
-            .range([h - padding, padding]); // Reverse to start bars from bottom
+            .domain([0, d3.max(filteredData, d => d.Value)])
+            .range([height, 0]);
 
-        // Create bars
-        svg.selectAll("rect")
-            .data(parsedData)
-            .enter()
-            .append("rect")
-            .attr("x", d => xScale(d.year))
-            .attr("y", d => yScale(d.value))
-            .attr("width", xScale.bandwidth())
-            .attr("height", d => h - padding - yScale(d.value))
-            .attr("fill", "steelblue");
+        // Define line generator
+        const line = d3.line()
+            .x(d => xScale(d.Year))
+            .y(d => yScale(d.Value));
+
+        // Draw lines
+        selectedCountries.forEach(country => {
+            const countryData = filteredData.filter(d => d.Country === country);
+            svg.append("path")
+                .datum(countryData)
+                .attr("fill", "none")
+                .attr("stroke", colorScale(country))
+                .attr("stroke-width", 2)
+                .attr("d", line);
+        });
 
         // Add x-axis
         svg.append("g")
-            .attr("transform", "translate(0," + (h - padding) + ")")
+            .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(xScale));
 
         // Add y-axis
         svg.append("g")
-            .attr("transform", "translate(" + padding + ",0)")
             .call(d3.axisLeft(yScale));
 
+        // Add chart title
+        svg.append("text")
+            .attr("x", (width / 2))
+            .attr("y", 0 - (margin.top / 2))
+            .attr("text-anchor", "middle")
+            .style("font-size", "16px")
+            .text("Total Fat Supply (FOODTFAT) in Selected Countries");
     });
 }
 
